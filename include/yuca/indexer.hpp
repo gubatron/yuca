dea19d0997cff951262d128c43a9a8509e83e898
gubatron <gubatron@gmail.com>
2016-11-20 00:16:01 -0700

    initial indexer code

--- include/yuca/indexer.hpp
+++ include/yuca/indexer.hpp
@@ -8,12 +8,63 @@
 
 namespace yuca {
 
+	struct ReverseIndex {
+		std::map<Key *, std::set<Document *>> index;
+
+		void put(Key *key, Document *doc) {
+			std::set<Document *> documents = get(key);
+			documents.insert(doc);
+		}
+
+		void addAll(KeySet keySet, Document *doc) {
+			KeySet::iterator keysIterator = keySet.begin();
+
+		    while (keysIterator != keySet.end()) {
+				Key* key = *keysIterator;
+				get(key);
+
+				keysIterator++;
+			}
+		}
+
+		bool has(Key *key) {
+			return index.count(key) > 0;
+		}
+
+		DocumentSet get(Key *key) {
+		    if (!has(key))	{
+				index[key] = std::set<Document *>();
+				return index[key];
+			}
+			std::map<Key *, std::set<Document *>>::iterator it = index.find(key);
+			// *it dereferences to a pair<Key*, std::vector<Document *>>
+			return (*it).second;
+		}
+	};
+
 	class Indexer {
 	public:
-		void indexDocument(Document &doc);
+		void indexDocument(const Document &doc);
 
 	private:
-		std::multimap<Key, Document*> reverse_index;
+		std::map<std::string, ReverseIndex> reverseIndices;
+
+		/**
+		 * The Indexer is confirmed by multiple reverse indexes,
+		 * which are identified by a 'tag', this helps us partition our indexing
+		 * by categories.
+		 *
+		 * For example, we could have a reverse index that catalogs documents by "file_extension"
+		 *
+		 * Inside the "file_extension" reverse index, we will find a multimap
+		 * of Key -> [Document, ...]
+		 *
+		 * Documents provide the indexer with the Keys to be used.
+		 *
+		 * */
+		ReverseIndex getReverseIndex(std::string const& tag);
+
+		void addToIndex(std::string const& tag, const Document &doc);
 	};
 }
 
--- src/indexer.cpp
+++ src/indexer.cpp
@@ -1,12 +1,52 @@
+#include <iostream>
 #include "yuca/indexer.hpp"
 
 namespace yuca {
+    void Indexer::indexDocument(const Document &doc) {
 
-	void Indexer::indexDocument(Document &doc) {
-        std::vector<Key> keys = doc.generateKeys();
-        for (Key key : keys) {
-            reverse_index.emplace(key, &doc);
-            reverse_index.insert(std::pair<Key, Document*>(key, &doc));
+        DocumentKeys keys = doc.getKeys();
+        DocumentKeys::iterator it = keys.begin();
+
+        // look for each one of the keys defined for this document
+        // the keys come along with their tag, which is used
+        // by the indexer to partition the reverse indexes
+        //
+        // Document -> { tagName -> [ Key, Key2, ..., KeyN ], ... }
+        //
+        //
+        // REVERSE_ENGINES[tag] ->
+        //      \_
+        //        [key1] = [Document1, Document2, ... ]
+        //        [key2] = [Document1, Document2, ... ]
+        while (it != keys.end()) {
+            std::string tag = it->first;
+            std::cout << "Indexer::indexDocument: tag [" << tag << "]";
+            ReverseIndex rIndex = getReverseIndex(tag);
+            addToIndex(tag, doc);
+            it++;
         }
-	}
+    }
+
+    void Indexer::addToIndex(std::string const& tag, const Document &doc) {
+        DocumentKeys keys = doc.getKeys();
+        DocumentKeys::iterator keysIterator = keys.find(tag);
+        if (keysIterator == keys.end()) {
+            std::cout << "Indexer::addToIndex(" << tag << "): check your logic, document does not use this tag" << std::endl;
+            return;
+        }
+        KeySet docKeys = keysIterator->second;
+        ReverseIndex rIndex = getReverseIndex(tag);
+        rIndex.addAll(docKeys, (Document *) &doc); // not sure if this will fly
+    }
+
+    ReverseIndex Indexer::getReverseIndex(std::string const& tag) {
+        if (reverseIndices.count(tag) == 0) {
+            // return a new map
+            reverseIndices[tag] = ReverseIndex();
+            return reverseIndices[tag];
+        }
+        std::map<std::string, ReverseIndex>::iterator
+                rIndexIterator = reverseIndices.find(tag);
+        return rIndexIterator->second;
+    }
 }
