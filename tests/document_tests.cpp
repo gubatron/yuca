f3673ed5d3ee6e1035f14288d11c9b78192575cf
gubatron <gubatron@gmail.com>
2017-02-19 18:05:10 -0700

    test with two keys

--- tests/document_tests.cpp
+++ tests/document_tests.cpp
@@ -12,22 +12,28 @@ public:
     virtual void SetUp() {
         fooTag = "foo:";
         fooKey = Key(fooTag);
+        barTag = "bar:";
+        barKey = Key(barTag);
         document = Document();
     }
 
     string fooTag;
-    Document document;
     Key fooKey;
+    string barTag;
+    Key barKey;
+    Document document;
 };
 
 
 TEST_F(DocumentTests, TestIfKeyCanBeAdded) {
     ;
     document.addKey(fooTag, fooKey);
+    document.addKey(fooTag, barKey);
+
     KeySet fooKeys = document.getTagKeys(fooTag);
 
     // make sure we have one
-    ASSERT_TRUE(fooKeys.size() == 1);
+    ASSERT_TRUE(fooKeys.size() == 2);
 
     // make sure it's the same one we've added
     KeySet::iterator it = fooKeys.find(&fooKey);
@@ -35,10 +41,17 @@ TEST_F(DocumentTests, TestIfKeyCanBeAdded) {
     ASSERT_FALSE(it == fooKeys.end());
 
     Key *isThisFooKeyPtr = *it;
-    std::cout << "key found? " << isThisFooKeyPtr->getTag() << std::endl;
+    std::cout << "tag found? " << isThisFooKeyPtr->getTag() << std::endl;
 
     ASSERT_TRUE(isThisFooKeyPtr->getTag() == fooTag);
     ASSERT_TRUE(isThisFooKeyPtr == (Key*) &fooKey);
+
+    it++;
+    Key *isThisBarKeyPtr = *it;
+    std::cout << "tag found? " << isThisBarKeyPtr->getTag() << std::endl;
+
+    ASSERT_TRUE(isThisBarKeyPtr->getTag() == barTag);
+    ASSERT_TRUE(isThisBarKeyPtr == (Key*) &barKey);
 }
 
 TEST_F(DocumentTests, TestsIfSingleKeyCanBeRemoved) {
