ca1179acb2479f18c1773d838309d7da1f75a01f
gubatron <gubatron@gmail.com>
2018-04-24 10:52:43 -0600

    tests_main.cpp only need include catch.hpp

--- tests/tests_main.cpp
+++ tests/tests_main.cpp
@@ -7,7 +7,6 @@
 
 // This tells Catch to provide a main() - only do this in one cpp file
 #define CATCH_CONFIG_MAIN
-
-#include "tests_includes.hpp"
+#include "catch.hpp"
 
 #endif //YUCA_TESTS_MAIN_H
