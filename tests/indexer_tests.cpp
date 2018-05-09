3cc98ea19f0457500902655eb1e3f068c0b93856
gubatron <gubatron@gmail.com>
2018-05-09 11:23:04 -0600

    License headers, LICENSE year interval update

--- tests/indexer_tests.cpp
+++ tests/indexer_tests.cpp
@@ -1,3 +1,27 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2016-2018 Angel Leon, Alden Torres
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 //
 // Created by gubatron on 02/21/2017.
 //
@@ -446,10 +470,13 @@ TEST_CASE("Indexer Search Tests") {
 		std::cout << search_result_sp.document_sp->intProperty("offset") << ". [" << search_result_sp.document_sp->stringProperty("full_name") << "], score: " << search_result_sp.score << std::endl;
 	}
 
+	REQUIRE(search_results_1.get(0).document_sp == doc_f2);
+	REQUIRE(search_results_1.size() == 10);
+
 	//std::cout << std::endl << "==============================" << std::endl << std::endl;
-	//std::cout << " Clear index!" << std::endl;
-	//indexer.clear();
-	//std::cout << indexer << std::endl;
+	//	std::cout << " Clear index!" << std::endl;
+	//	indexer.clear();
+	//	std::cout << indexer << std::endl;
 }
 
 #endif
