50be33ed4b6b57d3d63cf7626b46636b2054eb86
Alden Torres <aldenml@gmail.com>
2018-05-10 12:35:45 -0400

    fixed warning of using old style casting

--- src/yuca/key.cpp
+++ src/yuca/key.cpp
@@ -60,13 +60,13 @@ namespace yuca {
     }
 
     std::ostream &operator<<(std::ostream &output_stream, Key &key) {
-        output_stream << "Key(@" << ((long) &key % 10000) << ", id=" << key.id << ", tag=" << key.tag << ")";
+        output_stream << "Key(@" << (long(&key) % 10000) << ", id=" << key.id << ", tag=" << key.tag << ")";
         output_stream.flush();
         return output_stream;
     }
 
     std::ostream &operator<<(std::ostream &output_stream, StringKey &key) {
-        output_stream << "StringKey(@" << ((long) &key % 10000) << ", str_key=\"" << key.str_key << "\", id=" << key.id << ", tag=" << key.tag << ")";
+        output_stream << "StringKey(@" << (long(&key) % 10000) << ", str_key=\"" << key.str_key << "\", id=" << key.id << ", tag=" << key.tag << ")";
         output_stream.flush();
         return output_stream;
     }
