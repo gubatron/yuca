99939d727fd406fbafe901a94301a08fb9e38a4a
gubatron <gubatron@gmail.com>
2018-06-05 12:14:28 -0600

    [java] First part of Indexer java mapping

---
+++ swig/java/main/com/guacal/yuca/Indexer.java
@@ -0,0 +1,65 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2016-2018 Angel Leon, Alden Torres
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.guacal.yuca;
+
+public class Indexer {
+    private com.guacal.yuca.swig.Indexer swig;
+
+    public Indexer() {
+        swig = new com.guacal.yuca.swig.Indexer(); // implicit group will be :group
+    }
+
+    public Indexer(String implicitGroup) {
+        swig = new com.guacal.yuca.swig.Indexer(implicitGroup);
+    }
+
+    public void indexDocument(Document document) {
+        swig.indexDocument(document.swig());
+    }
+
+    public Document getDocument(int docId) {
+        return new Document(swig.getDocument(docId));
+    }
+
+    public Document getDocument(String docId) {
+        return new Document(swig.getDocument(docId));
+    }
+
+    boolean removeDocument(int docId) {
+        return swig.removeDocument(docId);
+    }
+
+    boolean removeDocument(String docId) {
+        return swig.removeDocument(docId);
+    }
+
+    boolean removeDocument(Document doc) {
+        return swig.removeDocument(doc.swig());
+    }
+
+    void clear() {
+        swig.clear();
+    }
+}
