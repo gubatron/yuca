7c5e0ae057da3c3e30593b78af3ddf36a687a441
gubatron <gubatron@gmail.com>
2018-06-06 14:53:40 -0600

    [java] Demo uses Yuca.loadLibrary
    build working in Linux now

--- swig/java/main/com/guacal/yuca/Demo.java
+++ swig/java/main/com/guacal/yuca/Demo.java
@@ -29,16 +29,8 @@ import com.guacal.yuca.swig.yuca;
 import java.util.List;
 
 public final class Demo {
-    static {
-        try {
-            // Temporary
-            System.load("/Users/gubatron/workspace.frostwire/yuca/swig/libyuca_jni_shared.dylib");
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
+	Yuca.loadLibrary();
         System.out.println("Yuca Java Wrapper version: " + yuca.version() + "\nLast Updated: " + yuca.getLAST_UPDATED() + "\n\n");
 
         StringKey fooKey = new StringKey("fooKey",":fooGroup");
